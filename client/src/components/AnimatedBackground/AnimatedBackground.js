8d8f620349b5d743b0f80e1550bc485a501bd7c4
DidiStf <dilyana.a.stefanova@gmail.com>
2021-01-20 16:42:51 +0100

    feat (client): home view and related components added.

---
+++ client/src/components/AnimatedBackground/AnimatedBackground.js
@@ -0,0 +1,18 @@
+import './AnimatedBackground.scss';
+
+const AnimatedBackground = () => {
+    return (
+        <div className="AnimatedBackground">
+            <ul className="AnimatedBackground_box-area">
+                <li className="AnimatedBackground_box-area_box box-1" />
+                <li className="AnimatedBackground_box-area_box box-2" />
+                <li className="AnimatedBackground_box-area_box box-3" />
+                <li className="AnimatedBackground_box-area_box box-4" />
+                <li className="AnimatedBackground_box-area_box box-5" />
+                <li className="AnimatedBackground_box-area_box box-6" />
+            </ul>
+        </div>
+    );
+};
+
+export default AnimatedBackground;
