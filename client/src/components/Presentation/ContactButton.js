a9e2de93733f046047a7248c0004a0b950ce4f97
DidiStf <dilyana.a.stefanova@gmail.com>
2021-01-22 23:06:08 +0100

    refactor (client): a tag replaced with a react-router-dom Link component

--- client/src/components/Presentation/ContactButton.js
+++ client/src/components/Presentation/ContactButton.js
@@ -1,9 +1,11 @@
+import { Link } from 'react-router-dom';
+
 import './ContactButton.scss';
 
 const ContactButton = () => {
     return (
         <div className="ContactButton">
-            <a href="#" className="ContactButton_link">Contact me</a>
+            <Link to="/contact" className="ContactButton_link">Contact me</Link>
         </div>
     );
 };
