4a183a2f2338009cb7e1d9d1e763ce29ac2cbdea
DidiStf <dilyana.a.stefanova@gmail.com>
2021-01-30 23:52:06 +0100

    feat (client): Skills view added

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,17 +1,19 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-import AboutView from "./views/About/About";
-import HomeView from "./views/Home/Home";
-import ContactView from "./views/Contact/Contact";
+import AboutView from './views/About/About';
+import HomeView from './views/Home/Home';
+import ContactView from './views/Contact/Contact';
+import SkillsView from './views/Skills/Skills';
 
 const Routes = () => {
   return (
     <>
       <Switch>
-        <Route exact path="/" component={HomeView} />
-        <Route exact path="/about" component={AboutView} />
-        <Route exact path="/contact" component={ContactView} />
+        <Route exact path='/' component={HomeView} />
+        <Route exact path='/about' component={AboutView} />
+        <Route exact path='/contact' component={ContactView} />
+        <Route exact path='/skills' component={SkillsView} />
       </Switch>
     </>
   );
---
+++ client/src/components/Skills/WordCloud.js
@@ -0,0 +1,98 @@
+import { useEffect, useState } from 'react';
+import SVG3DTagCloud from '3d-word-cloud';
+
+import './WordCloud.scss';
+
+const entries = [
+  { label: 'JavaScript' },
+  { label: 'HTML' },
+  { label: '(S)CSS' },
+  { label: 'SASS' },
+  { label: 'Bootstrap' },
+  { label: 'Materialize' },
+  { label: 'React' },
+  { label: 'React-Native' },
+  { label: 'Redux' },
+  { label: 'NodeJs' },
+  { label: 'Express' },
+  { label: 'Expo' },
+  { label: 'Firebase' },
+  { label: 'MongoDB' },
+  { label: 'PostgrSQL' },
+  { label: 'AdobeXd' },
+  { label: 'Figma' },
+  { label: 'Git' },
+  { label: 'Postman' },
+  { label: 'Linux' },
+  { label: 'Windows' },
+  { label: 'REST' },
+  { label: 'VueJs' },
+  { label: 'jQuery' },
+  /*{ label: 'Heroku' },
+  { label: 'Netlify' },
+  { label: 'GoogleCloud Platform' },
+  { label: "Jest" },
+  { label: "WebSockets" },
+  { label: "WebRTC" }
+  { label: "AWS" },
+  { label: "TypeScript" },
+  { label: "Php" },
+  { label: "Laravel" },
+  { label: "Selenuim" }*/
+];
+
+const SkillsWordCloud = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const settings = {
+    entries: entries,
+    width: windowWidth <= 1100 ? (windowWidth <= 550 ? 260 : 480) : 650,
+    height: windowWidth <= 1100 ? (windowWidth <= 550 ? 260 : 480) : 650,
+    radius: '55%',
+    minRadius: 0,
+    bgDraw: true,
+    bgColor: '#132c3a',
+    opacityOver: 1.0,
+    opacityOut: 0.3,
+    opacitySpeed: 6,
+    fov: 800,
+    speed: 0.1,
+    fontFamily: 'Oswald, Arial, sans-serif',
+    fontSize: windowWidth <= 550 ? '14' : '17',
+    fontColor: '#f5497d',
+    fontWeight: 'normal',
+    fontStyle: 'normal',
+    fontStretch: 'normal',
+    fontToUpperCase: false,
+  };
+
+  useEffect(() => {
+    const svg3DTagCloud = new SVG3DTagCloud(
+      document.getElementById('wordCloud'),
+      settings
+    );
+    svg3DTagCloud.animIn();
+
+    return () => {
+      svg3DTagCloud.animOut();
+      svg3DTagCloud.destroy();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setWindowWidth(window.innerWidth);
+      window.location.reload();
+    };
+
+    window.addEventListener('resize', handleWindowResize);
+    return () => {
+      window.removeEventListener('resize', handleWindowResize);
+    };
+  }, []);
+
+  return <div id='wordCloud' className='SkillsWordCloud'></div>;
+};
+
+export default SkillsWordCloud;
