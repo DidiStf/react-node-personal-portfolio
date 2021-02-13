34a356f0c0f8124d9939635ec580e3f7a18f44e4
DidiStf <dilyana.a.stefanova@gmail.com>
2021-02-13 12:10:38 +0100

    fix (client): " replaced with '

--- client/src/components/Presentation/ContactButton.js
+++ client/src/components/Presentation/ContactButton.js
@@ -3,11 +3,13 @@ import { Link } from 'react-router-dom';
 import './ContactButton.scss';
 
 const ContactButton = () => {
-    return (
-        <div className="ContactButton">
-            <Link to="/contact" className="ContactButton_link">Contact me</Link>
-        </div>
-    );
+  return (
+    <div className='ContactButton'>
+      <Link to='/contact' className='ContactButton_link'>
+        Contact me
+      </Link>
+    </div>
+  );
 };
 
 export default ContactButton;
