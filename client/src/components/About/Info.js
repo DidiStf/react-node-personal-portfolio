9b2c3a70dc03a6b8d0184373605357bd8025aaf3
DidiStf <dilyana.a.stefanova@gmail.com>
2021-02-19 20:43:32 +0100

    feat (client): about info translated

--- client/src/components/About/Info.js
+++ client/src/components/About/Info.js
@@ -1,33 +1,24 @@
+import { translate } from '../../utils/translate';
+
 import './Info.scss';
 
 const AboutInfo = () => {
   return (
     <div className='AboutInfo'>
-      <h3 className='AboutInfo_heading'>
-        Hi! I'm Dilyana and I'm a full-stack web developer based in Nice,
-        France.
-      </h3>
+      <h3 className='AboutInfo_heading'>{translate('about.presentation')}</h3>
       <p className='AboutInfo_info'>
-        I'm passionate about programming and languages. I love creating
-        aesthetic web and mobile applications and my goal is to write clean,
-        well-structured code and to improve myself constantly.
-        <br />
-        <br />I speak French, Spanish, English and Bulgarian fluently. I have a
-        Master's degree with honours in{' '}
-        <i>Hispanic and Latin American Studies</i> and a Bachelor's level
-        professional degree in{' '}
-        <i>Full-Stack Web and Mobile Application Development and Design</i>
-        .
+        {translate('about.info1')}
         <br />
         <br />
-        I mainly run on VS code and good coffee. I'm highly motivated and
-        passionate about learning new skills and best practices. I also like
-        keeping me up to date with the latest technologies and trying them by
-        building different projects.
+        {translate('about.info2')} <i>{translate('about.mastersDegree')}</i>
+        {translate('about.info3')} <i>{translate('about.bachelorsDegree')}</i>
         <br />
         <br />
-        <strong>Interests:</strong> video games, retro gaming consoles, board
-        games, electronics, languages and new technologies. Well, I'm a geek.
+        {translate('about.info4')}
+        <br />
+        <br />
+        <strong>{translate('about.interestsHeading')}</strong>
+        {translate('about.info5')}
       </p>
     </div>
   );
