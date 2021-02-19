17d9963d04e9650abc829bf1ff6ff26799eae0b9
DidiStf <dilyana.a.stefanova@gmail.com>
2021-02-19 20:43:16 +0100

    feat (client): navbar translated

--- client/src/components/Navbar/Menu.js
+++ client/src/components/Navbar/Menu.js
@@ -2,47 +2,52 @@ import classnames from 'classnames';
 import { useContext } from 'react';
 
 import { NavbarMenuContext } from '../../context/NavbarMenuProvider';
+import { TranslationLocaleContext } from '../../context/TranslationLocaleProvider';
 
 import NavbarItem from './Item';
 import NavbarMenuToggler from './Toggler';
+import FlagButtons from '../Buttons/FlagButtons/FlagButtons';
+
+import { translate } from '../../utils/translate';
 
 import './Menu.scss';
 
 const NavbarMenu = () => {
   const { open, toggleMenuAction } = useContext(NavbarMenuContext);
-  const pathCv = '/cv/CV_EN.pdf';
+  const { locale } = useContext(TranslationLocaleContext);
+  const pathCv = `/cv/CV_${locale.toUpperCase()}.pdf`;
 
   return (
     <div className={classnames('NavbarMenu', { open })}>
       <ul className='NavbarMenu_list'>
         <NavbarItem path='/' onClick={toggleMenuAction}>
-          Home
+          {translate('menu.home')}
         </NavbarItem>
         <NavbarItem path='/about' onClick={toggleMenuAction}>
-          About
+          {translate('menu.about')}
         </NavbarItem>
         <NavbarItem path='/skills' onClick={toggleMenuAction}>
-          Skills
+          {translate('menu.skills')}
         </NavbarItem>
         <NavbarItem path='/projects' onClick={toggleMenuAction}>
-          Projects
+          {translate('menu.projects')}
         </NavbarItem>
         <NavbarItem path='/experience' onClick={toggleMenuAction}>
-          Experience
+          {translate('menu.experience')}
         </NavbarItem>
         <NavbarItem path='/contact' onClick={toggleMenuAction}>
-          Contact
+          {translate('menu.contact')}
         </NavbarItem>
         <NavbarItem
           className='resume'
           path={pathCv}
-          onClick={toggleMenuAction}
           target='_blank'
           rel='noreferrer noopener'
         >
-          Resume
+          {translate('menu.resume')}
         </NavbarItem>
       </ul>
+      <FlagButtons />
       <NavbarMenuToggler />
     </div>
   );
