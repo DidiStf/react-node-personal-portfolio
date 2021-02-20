e4f661c11276903a71aa63dde441ea49b3d75aef
DidiStf <dilyana.a.stefanova@gmail.com>
2021-02-20 19:03:21 +0100

    fix: colors removed

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require('express');
 const dotenv = require('dotenv');
 const path = require('path');
 const morgan = require('morgan');
-const colors = require('colors');
 
 const contact = require('./routes/contact');
 
--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const ENV = process.env.NODE_ENV;
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () =>
-  console.log(`Server running in ${ENV} mode on port ${PORT}`.cyan.bold)
+  console.log(`Server running in ${ENV} mode on port ${PORT}`)
 );
