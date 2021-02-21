1bdd831b0d3751b069094d9cd8af89efa41dd0eb
DidiStf <dilyana.a.stefanova@gmail.com>
2021-02-21 02:54:36 +0100

    feat (client): logo added to navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,11 +2,14 @@ import NavbarMenu from './Menu';
 import NavbarMenuToggler from './Toggler';
 import TerminalAnimation from './TerminalAnimation';
 
+import logo from '../../assets/logo/logo.png';
+
 import './Navbar.scss';
 
 const Navbar = () => (
   <>
     <div className='Navbar'>
+      <img className='Navbar_logo' src={logo} alt='logo' />
       <TerminalAnimation path='/' />
       <NavbarMenuToggler />
     </div>
